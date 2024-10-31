da06b2acbb8cab3180e563342eb5f79cc43460db
Nicholas McCracken <nicholas.r.mccracken@gmail.com>
2024-10-31 13:46:28 -0400

    Improve error handling of data fetching and table rendering

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -3,25 +3,39 @@ import renderTable from './table-renderer.js';
 import createHoverEvents from './hover-events.js';
 
 /**
- * Fetches JSON data for courses, geneds, and semesters, then populates sequences and renders the table.
- * @returns {Promise<void>} A promise that resolves when the data is fetched and the table is rendered.
+ * Fetches data for courses, geneds, and semesters, then populates sequences and renders the table.
  */
-Promise.all([
-    fetch('/data/courses.json').then(response => response.json()),
-    fetch('data/geneds.json').then(response => response.json()),
-    fetch('data/semesters.json').then(response => response.json())
-])
-    .then(([courses, geneds, semesters]) => {
+async function initializeApp() {
+    try { 
+        const [courses, geneds, semesters] = await Promise.all([
+            fetchData('/data/courses.json'),
+            fetchData('data/geneds.json'),
+            fetchData('data/semesters.json')
+        ]);
         console.log('JSON data was fetched successfully.');
 
-        try {
-            const populatedCourses = populateSequences(courses);
-            renderTable(populatedCourses, geneds, semesters);
-            createHoverEvents();
+        // Populate pre/postreq sequences and render the curriculum table
+        const populatedCourses = populateSequences(courses);
+        renderTable(populatedCourses, geneds, semesters);
+        createHoverEvents();
 
-            console.log('Table was rendered successfully.');
-        } catch (error) {
-            console.error('Error rendering table:\n', error);
-        }
-    })
-    .catch(error => console.error('Error loading JSON file:\n', error));
+        console.log('Table was rendered successfully.');
+    } catch(error) {
+        console.error('An error occured:\n', error);
+    }
+}
+
+/**
+ * Fetches JSON data from a given URL and returns the parsed data.
+ * @param {string} url - The URL to fetch data from.
+ * @returns {Promise<Object>} A promise that resolves to the parsed JSON data.
+ */
+async function fetchData(url) {
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(`Failed to fetch data from ${url}: ${response.statusText}`);
+    }
+    return response.json();
+}
+
+initializeApp();
